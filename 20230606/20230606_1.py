a9fad7bf7bb21305f424b2436355c78499a23571
jaekim3220 <jehyunkim9@gmail.com>
2023-06-02 22:30:53 +0900

    write (20230606_1.py) but not run

---
+++ 20230606/20230606_1.py
@@ -0,0 +1,8 @@
+'''숫자 카드 게임
+각 행에서 가장 작은 숫자를 찾는다
+가장 작은 숫자들 중에서 가장 큰 숫자를 선택한다'''
+def card(mycards):
+    card_list = [] #
+    for a in range(len(mycards)):
+        card_list.append(a)
+    return card_list
