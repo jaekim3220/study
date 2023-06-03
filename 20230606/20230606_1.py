6b4e5b6f9969d89fe4ec21cf6b17a12bd24c0aea
jaekim3220 <jehyunkim9@gmail.com>
2023-06-03 10:36:20 +0900

    card_list.append(a) 변경

--- 20230606/20230606_1.py
+++ 20230606/20230606_1.py
@@ -4,5 +4,10 @@
 def card(mycards): 
     card_list = [] # 각 행의 최소 값 저장 list
     for a in mycards: # 입력 받은 'mycards 내부 요소'를 card_list에 입력 해야 함
-        card_list.append(a) # 추출한 값을 card_list에 입력
+        card_list.append(min(a)) # 추출한 최소 값을 card_list에 입력
     return card_list
+
+cards = [[3, 1, 2], 
+         [4, 1, 4], 
+         [2, 2, 2]]
+print(card(cards))
