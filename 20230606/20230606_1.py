b552dc04ff2725f276e27531fc2d030cc76ac422
jaekim3220 <jehyunkim9@gmail.com>
2023-06-03 15:25:21 +0900

    return card_list 변경

--- 20230606/20230606_1.py
+++ 20230606/20230606_1.py
@@ -5,9 +5,14 @@ def card(mycards):
     card_list = [] # 각 행의 최소 값 저장 list
     for a in mycards: # 입력 받은 'mycards 내부 요소'를 card_list에 입력 해야 함
         card_list.append(min(a)) # 추출한 최소 값을 card_list에 입력
-    return card_list
+    return max(card_list) # card_list에서 가장 큰 값을 출력
 
-cards = [[3, 1, 2], 
+cards1 = [[3, 1, 2], 
          [4, 1, 4], 
          [2, 2, 2]]
-print(card(cards))
+print(f"결과 : {card(cards1)}")
+
+# cards2 = [[8, -80, 23], 
+#          [23, 71, -10], 
+#          [-5, 100, 86]]
+# print(f"결과 : {card(cards2)}")
