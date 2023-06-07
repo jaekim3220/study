2078ec1cb7802492736edcc66df1f36cff04db2f
jaekim3220 <jehyunkim9@gmail.com>
2023-06-07 18:55:57 +0900

    so1, so2, save를 추가했습니다.

--- GitHub/GitHub.py
+++ GitHub/GitHub.py
@@ -1,1 +1,4 @@
 what_is_commit = 'commit은 본인의 활동, 변경 내용의 한줄 요약입니다.'
+so1 = 'commit을 활용해 변경 사항을 확인하고 comment를 남기고 공유할 수 있습니다.'
+so2 = '변경 사항, 의견을 빠르고 편리한 공유하기 위해서는 github에서 commit을 잘 활용해야합니다.'
+save = 'commit 하기 전 save 하는 걸 잊지 마세요. ^^'
