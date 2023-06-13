e2b808d068361c8a009331ecf326a47b7751679f
jaekim3220 <jehyunkim9@gmail.com>
2023-06-13 21:02:29 +0900

    수정3 : 계산 확인용 print 추가 - 논리 오류

--- 20230613/20230613_2.py
+++ 20230613/20230613_2.py
@@ -7,10 +7,12 @@ def calculate(S):
     answer = int(S[0]) # 초기값은 입력한 문자열 S의 첫 번째 값
     for a in range(1, len(S)): # 1~S의 길이만큼 연산을 진행
         number = int(S[a]) # a 번째에 있는 숫자
-        if number == 0: 
+        if number != 0 or number<1: 
             answer += number # a번째 숫자가 0일 경우 + 연산
+            print(answer)
         else:
             answer *= number # a번째 숫자가 0일 경우 * 연산
+            print(answer)
     return answer
 S = (0, 2, 9, 8, 4)
 print(int(calculate(S)))
