b6188b2b5a4d35aad9805eb7b7bf051422667598
jaekim3220 <jehyunkim9@gmail.com>
2023-06-13 13:47:13 +0900

    수정2 : 변수 추가 및 if문 변경

--- 20230613/20230613_2.py
+++ 20230613/20230613_2.py
@@ -6,10 +6,11 @@
 def calculate(S):
     answer = int(S[0]) # 초기값은 입력한 문자열 S의 첫 번째 값
     for a in range(1, len(S)): # 1~S의 길이만큼 연산을 진행
-        if a != 0:
-            answer *= a
+        number = int(S[a]) # a 번째에 있는 숫자
+        if number == 0: 
+            answer += number # a번째 숫자가 0일 경우 + 연산
         else:
-            answer += a
+            answer *= number # a번째 숫자가 0일 경우 * 연산
     return answer
 S = (0, 2, 9, 8, 4)
 print(int(calculate(S)))
