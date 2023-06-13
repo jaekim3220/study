fab1b43d1c64c60ef6eef27b2199e23235c09523
jaekim3220 <jehyunkim9@gmail.com>
2023-06-13 21:15:36 +0900

    수정4 : if 조건 변경 - 최종?

--- 20230613/20230613_2.py
+++ 20230613/20230613_2.py
@@ -7,12 +7,13 @@ def calculate(S):
     answer = int(S[0]) # 초기값은 입력한 문자열 S의 첫 번째 값
     for a in range(1, len(S)): # 1~S의 길이만큼 연산을 진행
         number = int(S[a]) # a 번째에 있는 숫자
-        if number != 0 or number<1: 
+        if number == 0 or answer==0: 
             answer += number # a번째 숫자가 0일 경우 + 연산
-            print(answer)
+            # 초기 값과 a 번째(1~len(S)사이) 값이 0인 경우 + 연산을 진행
         else:
             answer *= number # a번째 숫자가 0일 경우 * 연산
-            print(answer)
     return answer
 S = (0, 2, 9, 8, 4)
-print(int(calculate(S)))
+print(f"S 결과 : {int(calculate(S))}")
+S1 = (9, 0, 2, 3, 7, 5)
+print(f"S1 결과 : {int(calculate(S1))}")
