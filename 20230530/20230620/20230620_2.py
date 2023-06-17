8abe2ba6b81caa308141924c6ed5e354bebb2a11
jaekim3220 <jehyunkim9@gmail.com>
2023-06-17 11:30:10 +0900

    최종 제출

--- 20230530/20230620/20230620_2.py
+++ 20230530/20230620/20230620_2.py
@@ -8,16 +8,32 @@
     위의 계산을 모든 경우에 대해 반복하여 경우의 수를 누적
 '''
 def bowling(N,M,weights):
-    count = 0 # 볼링공을 고르는 경우의 수 초기 값
+    count = 0 # 공을 고르는 경우의 수 초기 값
     weight_dict = {} # 공의 무게 목록(무게별 공의 개수)
     
     # 무게별 공의 개수를 dict 형태로 확인
     for a in weights:
-        print(weight_dict)
-        if a in weight_dict:
+        # print(weight_dict)
+        if a in weight_dict: # dict에 해당 무게가 있으면 +1
             weight_dict[a] += 1
-        else:
+        else: # dict에 해당 무게가 없으면 1로 설정(초기)
             weight_dict[a] = 1
-    return weight_dict
-weights = [1, 3, 2, 3, 2] 
-print(bowling(weights))
+
+    # 공을 선택하는 경우의 수 N
+    # 공의 수 N, 공의 무게 : 1~M
+    for i in range(1,M+1): # 1~M
+        if i in weight_dict: # i라는 무게의 공이 존재할 경우
+            N -= weight_dict[i] # 선택 후 남은 공의 개수
+            # 경우의 수
+            count += weight_dict[i] * N  # 현재 무게의 공과 다른 무게의 공들의 조합 수
+    return count
+# 예시 1
+N1 = 5  
+M1 = 3  
+weights1 = [1, 3, 2, 3, 2]  
+print(bowling(N1,M1,weights1))
+# 예시 1
+N2 = 8  
+M2 = 5  
+weights2 = [1,5,4,3,2,4,5,2]  
+print(bowling(N2,M2,weights2))
