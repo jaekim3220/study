8cce4f307a4d587a07c84f9d51d1e7f738743f66
jaekim3220 <jehyunkim9@gmail.com>
2023-07-28 15:25:54 +0900

    문제해결 bestPercent 변수 위치로 인한 초기화 문제였음.

--- 20230801/!로또의_최고_순위.py
+++ 20230801/!로또의_최고_순위.py
@@ -6,9 +6,12 @@
 '''
 
 def solution(lottos, win_nums):
+    # bestPercent = NumCount + zeroCount  #당첨 확률이 가장 높은 수
+    # NumCount와 zeroCount를 계산하기 전에 초기화
+    # 결국 초기에 0으로 설정되고 이후에 갱신되지 않기 때문에 항상 0
+
     NumCount = 0    #0이 아닌 숫자 중 일치하는 개수용
     zeroCount = 0   #0인 숫자의 일치하는 개수용
-    bestPercent = NumCount + zeroCount  #당첨 확률이 가장 높은 수
     rank = {6:1, 5:2, 4:3, 3:4, 2:5, 1:6, 0:6}  #당첨 개수와 순위
 
     for i in range(len(lottos)):
@@ -16,11 +19,18 @@ def solution(lottos, win_nums):
             NumCount += 1
         if lottos[i] == 0:  #0인 숫자
             zeroCount += 1
+    bestPercent = NumCount + zeroCount
     answer = [rank[bestPercent], rank[NumCount]]  #0인 숫자가 반드시 틀릴 경우 확률이 가장 낮다
     return answer
 
-lottos1, lottos2, lottos3 = [44, 1, 0, 0, 31, 25], [0, 0, 0, 0, 0, 0], [45, 4, 35, 20, 3, 9]
-win_nums1, win_nums2, win_nums3 = [31, 10, 45, 1, 6, 19], [38, 19, 20, 40, 15, 25], [20, 9, 3, 45, 4, 35]
-print("1 : ", solution(lottos1, win_nums1))
-print("2 : ", solution(lottos2, win_nums2))
-print("3 : ", solution(lottos3, win_nums3))
+lottos1 = [44, 1, 0, 0, 31, 25]
+win_nums1= [31, 10, 45, 1, 6, 19]
+print(solution(lottos1, win_nums1))
+
+# lottos2 = [0, 0, 0, 0, 0, 0]
+# win_nums2 = [38, 19, 20, 40, 15, 25] 
+# print(solution(lottos2, win_nums2))
+
+# lottos3 = [45, 4, 35, 20, 3, 9]
+# win_nums3 = [20, 9, 3, 45, 4, 35]
+# print(solution(lottos3, win_nums3))
