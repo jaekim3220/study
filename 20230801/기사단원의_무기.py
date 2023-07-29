f03b13284ef58e7f3ea3c26d1db7e2a9318b74ac
jaekim3220 <jehyunkim9@gmail.com>
2023-07-29 19:05:08 +0900

    기사단원의 무기 성공

--- 20230801/기사단원의_무기.py
+++ 20230801/기사단원의_무기.py
@@ -5,8 +5,11 @@ def solution(number, limit, power):
         for j in range(1, int(i ** (1 / 2)) + 1):  # 1부터 i의 제곱근까지
             if i % j == 0:
                 div += 1
+                if j ** 2 != i:  # 제곱이 되는 약수는 중복 방지.
+                    div += 1
             if div > limit:  # limit값을 초과하면 power
                 div = power
+                break
         divList.append(div)
     return sum(divList)
 
