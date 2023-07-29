39df5ea4f31b67bd2dae59f73d02827a0901b1d0
jaekim3220 <jehyunkim9@gmail.com>
2023-07-29 14:51:32 +0900

    숫자 짝궁 실패 2

--- 20230801/숫자_짝꿍.py
+++ 20230801/숫자_짝꿍.py
@@ -1,14 +1,17 @@
 def solution(X, Y):
     answer = ''
-    sameNum = ''
-    for i in range(len(X)):
-        if X[i] in Y:
-            sameNum += X[i]
-    sameNum = sorted(sameNum, reverse=True)
-    if len(sameNum) == 0:
+    for num in range(10):
+        count_x = X.count(str(num))
+        count_y = Y.count(str(num))
+        count_min = min(count_x, count_y)
+        if count_min > 0:
+            answer += str(num) * count_min
+
+    if not answer:
         answer = "-1"
     else:
-        answer = ''.join(sameNum)
+        answer = ''.join(sorted(answer, reverse=True))
+
     return answer
 
 X = "100"
