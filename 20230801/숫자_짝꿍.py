f555ef37cf0c3bc051072b59b092fc96bdc5e5e3
jaekim3220 <jehyunkim9@gmail.com>
2023-08-02 09:41:54 +0900

    숫자 짝꿍 - 실패

--- 20230801/숫자_짝꿍.py
+++ 20230801/숫자_짝꿍.py
@@ -1,13 +1,13 @@
 def solution(X, Y):
     answer = ''
     for num in range(10):
-        count_x = X.count(str(num))
-        count_y = Y.count(str(num))
-        count_min = min(count_x, count_y)
-        if count_min > 0:
-            answer += str(num) * count_min
+        count_x = X.count(str(num)) # X의 숫자를 확인
+        count_y = Y.count(str(num)) # Y의 숫자를 확인
+        count_min = min(count_x, count_y)   # 공통 숫자를 찾는 만큼 짧은걸 선택한다
+        if count_min > 0:   # 공통 숫자가 있는 경우
+            answer += str(num) * count_min  # 공통 숫자를 결과 문자열에 추가합니다. (예: 5가 3번 등장하면 '555'를 추가)
 
-    if not answer:
+    if not answer:  # 공통 숫자 없으면 -1 반환
         answer = "-1"
     else:
         answer = ''.join(sorted(answer, reverse=True))
