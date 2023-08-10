03f38013332998602bdf8a07f0355d2cd4a61a71
jaekim3220 <jehyunkim9@gmail.com>
2023-08-10 19:12:54 +0900

    갱신

--- 20230808/!체육복.py
+++ 20230808/!체육복.py
@@ -22,3 +22,48 @@ def solution(n, lost, reserve):
 print(solution(5, [2, 4], [1, 3, 5]))   #5
 print(solution(5, [2, 4], [3])) #4
 print(solution(3, [3], [1]))    #2
+
+# #------------------------------
+# # 지선님 풀이
+# #------------------------------
+
+n = 5 	# 전체학생
+lost  = [3,5]	# 도난
+reserve	= [2,4]	# 여벌 
+
+# 학생들 체육복 보유 상황
+students = [1 for i in range(n+1)]
+for a in lost : # 도난 당한 애들 
+    students[a] = 0 
+for b in reserve : # 여분 있는 애들
+    students[b] += 1 
+real = students[1:]  
+real2 = students[1:]  
+#real
+
+# 앞에서부터 없는 애들 채우기
+for i in range(len(real)-1) :
+    if real[i] == 0 and real[i+1] == 2 :
+        real[i] = 1
+        real[i+1] = 1 
+
+for i in range(len(real)-1) :
+    if real[i] == 2 and real[i+1] == 0 :
+        real[i] = 1
+        real[i+1] = 1 
+#real
+
+# 뒤에서부터 없는 애들 채우기 
+for i in range(len(real2)-1) :
+    if real2[i] == 2 and real2[i+1] == 0 :
+        real2[i] = 1
+        real2[i+1] = 1 
+for i in range(len(real2)-1) :
+    if real2[i] == 0 and real2[i+1] == 2 :
+        real2[i] = 1
+        real2[i+1] = 1 
+#real2
+
+# 가장 많이 채울 수 있는 걸로 사용하기
+answer = n - min(real.count(0),real2.count(0))
+answer
--- 20230808/대충_만든_자판.py
+++ 20230808/대충_만든_자판.py
@@ -28,3 +28,25 @@ def solution(keymap, targets):
 print(solution(["ABACD", "BCEFD"], ["ABCD","AABB"]))  # [9, 4]
 print(solution(["AA"], ["B"]))  # [-1]
 print(solution(["AGZ", "BSSS"], ["ASA","BGZ"]))  # [4, 6]
+
+# #------------------------------
+# # 인수님 풀이
+# #------------------------------
+def solution(keymap,targets):
+    temp = [0] * len(targets)
+    my_dict = dict()    
+    for i in range(len(keymap)):
+        for k in range(len(keymap[i])):
+            if keymap[i][k] not in my_dict:
+                my_dict[keymap[i][k]] = keymap[i].index(keymap[i][k]) + 1 # 몇번째 key인지 값을 넣음
+            else:
+                if my_dict[keymap[i][k]] > keymap[i].index(keymap[i][k]) + 1:   # 더 크면 버리고 대체
+                    my_dict[keymap[i][k]] = keymap[i].index(keymap[i][k]) + 1
+    for i in range(len(targets)):
+        for j in range(len(targets[i])):
+            if targets[i][j] in my_dict: # 타겟 값이 딕셔너리에 있다면
+                temp[i] += my_dict[targets[i][j]] # 리스트의 i번째 장소에 i번째 target의 값들을 넣어서 저장
+            else:
+                temp[i] = -1    # 하나라도 발견이 안되면 -1로 처리
+                break
+    return temp
