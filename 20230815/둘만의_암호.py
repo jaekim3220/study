9a6c0a80f85774690ad61e4cb1bd6d182837acdf
jaekim3220 <jehyunkim9@gmail.com>
2023-08-20 13:12:02 +0900

    둘만의 암호 성공

--- 20230815/둘만의_암호.py
+++ 20230815/둘만의_암호.py
@@ -8,13 +8,12 @@ def solution(s, skip, index):
     result = ""
 
     for i in skip:  # 문자가 skip에 포함된 경우
-        if i in s:
+        if i in answer: # skip에 포함된 문자가 answer에 있으면
             answer = answer.replace(i,"")   # skip에 포함된 문자 삭제,str은 pop 사용 불가
             
-    
     for j in s:
-        result += answer[(answer.index(j) + index) % len(answer)]
-
+        result += answer[(answer.index(j) + index) % len(answer)]   #% len(answer)을 통해 answer의 범위를 벗어나는 것을 방지
+    # print("answer길이:",len(answer))
     return result
 
 print((solution("aukks", "wbqd", 5)))   #"happy"
